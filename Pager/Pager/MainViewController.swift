de0e3a2d3227fc8bffb325a78612c5a9f4c06ea0
Vignesh Ramesh <r.vigs23@gmail.com>
2015-06-29 23:04:11 -0700

    Initial setup of multiple storyboards.

---
+++ Pager/Pager/MainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewController.swift
+//  Pager
+//
+//  Created by Vignesh Ramesh on 6/29/15.
+//  Copyright (c) 2015 SOMA. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let delay = 3 * Double(NSEC_PER_SEC)
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        dispatch_after(time, dispatch_get_main_queue()) {
+            self.gotoExploreVC()
+        }
+    }
+    
+    func gotoExploreVC() {
+        var storyboard = UIStoryboard(name: "Explore", bundle: nil)
+        var controller = storyboard.instantiateViewControllerWithIdentifier("ExploreVC") as! ExploreViewController
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pager/Pager/SearchViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SearchViewController.swift
+//  Pager
+//
+//  Created by Vignesh Ramesh on 6/29/15.
+//  Copyright (c) 2015 SOMA. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func exploreBtnDidPress(sender: AnyObject) {
+    
+        var storyboard = UIStoryboard(name: "Explore", bundle: nil)
+        var controller = storyboard.instantiateViewControllerWithIdentifier("ExploreVC") as! ExploreViewController
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+
+}
